88ebbf9c2363d37767fdd872fd36a8bbbab15b13
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2024-11-05 02:25:58 +0530

    Custom component create

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,14 @@
-import { StyleSheet, Text, View, Image} from "react-native";
-import { Colors } from "@/constants/Colors";
+import { StyleSheet, Text, View, } from "react-native";
 import products from "@/assets/data/products";
+import ProductListItem from "@/components/ProductListItem";
 
-const product = products[0];
-
-export default function App(){
+export default function MenuScreen(){
   return(
-    <View style = {styles.container}>
-      <Image source={{uri:product.image}} style={styles.Image}/>
-      <Text style = {styles.title}>{product.name}</Text>
-      <Text style ={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product ={products[0]}/>
+      <ProductListItem product ={products[1]}/>
+      <ProductListItem product ={products[2]}/>
     </View>
-  )
-}
 
-const styles = StyleSheet.create({
-  container:{
-    backgroundColor:'white',
-    padding:10,
-    borderRadius:15,
-  },
-  title:{
-    fontSize:18,
-    fontWeight:600,
-    marginVertical:10,
-  },
-  price:{
-    color: Colors.light.tint,
-    fontWeight:'bold',
-  },
-  Image:{
-    width:'100%',
-    aspectRatio:1,
-  },
-})
+  );
+}
---
+++ components/ProductListItem.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View, Image} from "react-native";
+import { Colors } from "@/constants/Colors";
+import products from "@/assets/data/products";
+
+const ProductListItem =({product}) => {
+  return(
+    <View style = {styles.container}>
+      <Image source={{uri:product.image}} style={styles.Image}/>
+      <Text style = {styles.title}>{product.name}</Text>
+      <Text style ={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:'white',
+    padding:10,
+    borderRadius:15,
+  },
+  title:{
+    fontSize:18,
+    fontWeight:600,
+    marginVertical:10,
+  },
+  price:{
+    color: Colors.light.tint,
+    fontWeight:'bold',
+  },
+  Image:{
+    width:'100%',
+    aspectRatio:1,
+  },
+})
